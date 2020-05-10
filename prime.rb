ea6c6aebe774e9833599126c0c45395e49b8ce14
Ben Parish <parishben13@gmail.com>
2020-05-10 20:30:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 # Add  code here!
 def prime?(some_Int)
-  (2..(some_Int - 1)).each do |n|
+  if some_Int.is_a
+    (2..(some_Int - 1)).each do |n|
     return false if some_Int % n == 0
   end
   true
